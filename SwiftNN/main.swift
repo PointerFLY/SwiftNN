f7454ac6d727f3c8a408f7709d6395e34e14c380
PointerFLY <linghao.michael.ma@gmail.com>
2019-12-28 09:22:38 +0800

    Initialize project

---
+++ SwiftNN/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftNN
+//
+//  Created by PointerFLY on 28/12/19.
+//  Copyright © 2019 PointerFLY. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
